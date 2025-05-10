89e39f1297bc3542612b4603bb918823a6013fff
Ayush-CS-19 <ayushkaushik257@gmail.com>
2025-05-10 22:46:09 +0530

    Change env

--- Frontend/src/utils/constants.jsx
+++ Frontend/src/utils/constants.jsx
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const JOBS_API_END_POINT = "http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+const backend_domain = import.meta.env.VITE_BACKEND_URL;
+export const USER_API_END_POINT = `${backend_domain}/api/v1/user`;
+export const JOBS_API_END_POINT = `${backend_domain}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${backend_domain}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${backend_domain}/api/v1/company`;
