be43f377a04dc3aa39b9ffab95f06a3c39271ad7
Ayush-CS-19 <145976736+Ayush-CS-19@users.noreply.github.com>
2026-01-02 00:47:07 +0530

    Update useGetallCompany.jsx

--- Frontend/src/hooks/useGetallCompany.jsx
+++ Frontend/src/hooks/useGetallCompany.jsx
@@ -1,26 +1,31 @@
 import { useEffect } from "react";
 import axios from "axios";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { COMPANY_API_END_POINT } from "../utils/constants";
 import { setCompanies } from "../redux/companySlice";
+
 const useGetallCompany = () => {
   const dispatch = useDispatch();
+  const { searchQuery } = useSelector((store) => store.job); 
 
   useEffect(() => {
     const fetchAllCompany = async () => {
       try {
-        const res = await axios.get(`${COMPANY_API_END_POINT}/get`, {
-          withCredentials: true,
-        });
+        const res = await axios.get(
+          `${COMPANY_API_END_POINT}/get?keyword=${searchQuery}`,
+          { withCredentials: true }
+        );
+
         if (res.data.success) {
           dispatch(setCompanies(res.data.companies));
         }
       } catch (err) {
-        console.log(err);
+        console.error(err);
       }
     };
+
     fetchAllCompany();
-  }, []);
+  }, [dispatch, searchQuery]); 
 };
 
 export default useGetallCompany;
